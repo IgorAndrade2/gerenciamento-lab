3e18921a29475cb86384bbc0d1de07ace957ea5c
IgorAndrade2 <igorandradeferreira@gmail.com>
2025-11-25 15:58:36 -0300

    controllers ajustes

--- src/main/java/com/senai/backend/gerenciamento_lab/controllers/EquipamentoController.java
+++ src/main/java/com/senai/backend/gerenciamento_lab/controllers/EquipamentoController.java
@@ -8,13 +8,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.senai.backend.gerenciamento_lab.models.Equipamento;
 import com.senai.backend.gerenciamento_lab.services.EquipamentoService;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 
 @RestController
--- src/main/java/com/senai/backend/gerenciamento_lab/controllers/LaboratorioController.java
+++ src/main/java/com/senai/backend/gerenciamento_lab/controllers/LaboratorioController.java
@@ -8,11 +8,16 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.senai.backend.gerenciamento_lab.models.Laboratorio;
 import com.senai.backend.gerenciamento_lab.services.LaboratorioService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
+@RestController
+@RequestMapping("/laboratorio")
 public class LaboratorioController {
 
     @Autowired
